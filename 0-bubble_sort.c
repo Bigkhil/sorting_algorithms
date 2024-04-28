3b8eb25fe4b08822d49290eab574844c85508b6e
Mohamed khalil <khm40005000@outlook.com>
2024-04-28 16:24:25 +0300

    some changes

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,7 +7,7 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-	int check = 0, temp = 0;
+	int temp = 0;
 	size_t i = 0, j = 0;
 
 	for (i = 0 ; i < (size - 1) ; i++)
@@ -19,7 +19,6 @@ void bubble_sort(int *array, size_t size)
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
-				check = 1;
 				print_array(array, size);
 			}
 		}
